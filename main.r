724906343b0dc3bfd4e49a657ccf27f29bd8828f
ramzy05 <ramsesaguegang@gmail.com>
2022-12-24 14:23:36 +0100

    creation des variables train, et test dans main.r qui contienent respectivement le jeu d'entrainement et le jeu de test

--- main.r
+++ main.r
@@ -8,11 +8,17 @@ df = read.csv(file = 'dataset_credit_card.csv',sep=',',header = T,na.strings = '
 #D'après la commande suivante il n'y a pas de valeurs manquantes
 #print(paste('Number of missing values =',sum(is.na(df))))
 
-
+.
 
 #pretaitement
   df = pretaitement(df)
     #summary(df)
+  set.seed(1)
+
+  #u 70% des données pour training set et 30% pour test set
+  sample <- sample(c(TRUE, FALSE), nrow(df), replace=TRUE, prob=c(0.7,0.3))
+  train  <- df[sample, ]
+  test   <- df[!sample, ]
 
 #extraction des règles
   rules = extract_rules(df)
